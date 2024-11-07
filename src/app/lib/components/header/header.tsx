5de7972835343be6b81eb435c96eee01483da8df
Akshat42 <akshatdivya03@gmail.com>
2024-11-08 00:00:15 +0530

    feat: stati header

--- src/app/lib/components/header/header.tsx
+++ src/app/lib/components/header/header.tsx
@@ -1,8 +1,19 @@
+import BurgerMenu from '@assets/icons/menu.svg';
+import Image from 'next/image';
+import Link from 'next/link';
+
 const Header: React.FC = () => {
   return (
-    <div className="bg-orange-500 py-5">
-      <h1 className="text-center text-xl">Header</h1>
-    </div>
+    <header className="flex flex-row justify-start items-center bg-slate-900">
+      <Link href="/" className="p-3">
+        <Image
+          src={BurgerMenu}
+          alt="burger_menu"
+          className="bg-slate-200 lg:w-8"
+        />
+      </Link>
+      <h1 className="text-xl lg:text-3xl	text-white">ALGO VISUALIZER</h1>
+    </header>
   );
 };
 
