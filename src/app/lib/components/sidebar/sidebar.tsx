18fdb6b96b3c00667406eb78c4d44a8d0b74a7ca
Akshat42 <akshatdivya03@gmail.com>
2024-11-20 23:24:26 +0530

    fix: sidebar border

--- src/app/lib/components/sidebar/sidebar.tsx
+++ src/app/lib/components/sidebar/sidebar.tsx
@@ -9,11 +9,11 @@ const Sidebar: React.FC = () => {
   const { isSideBarOpen } = useSidebarStore();
   return (
     isSideBarOpen && (
-      <aside className="h-full w-[22vw] absolute">
+      <aside className="h-full w-[15vw] absolute">
         <div className="h-full bg-gray-800 text-white overflow-auto pt-14">
           {NavLinkList.map((navCategory) => {
             return (
-              <div className="p-3">
+              <div className="p-3 border-t-4 border-gray-600">
                 <div className="flex gap-2 p-1">
                   <IconMenu3 stroke={2} />
                   <h2 className="text-2xl font-bold">
