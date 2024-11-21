79329810aaae92ad16bafb32bc5bff9e25a714f3
Akshat42 <akshatdivya03@gmail.com>
2024-11-21 19:35:05 +0530

    toggle sidebar function

--- src/app/lib/components/sidebar/sidebar.tsx
+++ src/app/lib/components/sidebar/sidebar.tsx
@@ -7,28 +7,32 @@ import { useSidebarStore } from 'lib/store/sidebar.store';
 
 const Sidebar: React.FC = () => {
   const { isSideBarOpen } = useSidebarStore();
+
+  function getSidebarClasses() {
+    if (isSideBarOpen) {
+      return 'h-full min-w-40 w-[15vw] absolute opacity-100 translate-y-0';
+    } else {
+      return 'hidden opacity-0 opacity-0 -translate-y-4';
+    }
+  }
   return (
-    isSideBarOpen && (
-      <aside className="h-full w-[15vw] absolute">
-        <div className="h-full bg-gray-800 text-white overflow-auto pt-14">
-          {NavLinkList.map((navCategory) => {
-            return (
-              <div className="p-3 border-t-4 border-gray-600">
-                <div className="flex gap-2 p-1">
-                  <IconMenu3 stroke={2} />
-                  <h2 className="text-2xl font-bold">
-                    {navCategory.headerText}
-                  </h2>
-                </div>
-                {navCategory.subMenu.map((subMenuItem) => {
-                  return <SidebarMenuItem menuItem={subMenuItem} />;
-                })}
+    <aside className={getSidebarClasses()}>
+      <div className="h-full bg-gray-800 text-white overflow-auto pt-14 ">
+        {NavLinkList.map((navCategory) => {
+          return (
+            <div className="p-3 border-t-4 border-gray-600 ">
+              <div className="flex gap-2 p-1">
+                <IconMenu3 stroke={2} />
+                <h2 className="text-2xl font-bold">{navCategory.headerText}</h2>
               </div>
-            );
-          })}
-        </div>
-      </aside>
-    )
+              {navCategory.subMenu.map((subMenuItem) => {
+                return <SidebarMenuItem menuItem={subMenuItem} />;
+              })}
+            </div>
+          );
+        })}
+      </div>
+    </aside>
   );
 };
 
