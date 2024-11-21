43e108568faf4a006dec07396178378925181bfd
Akshat42 <akshatdivya03@gmail.com>
2024-11-21 20:23:45 +0530

    feat: footer

--- src/app/lib/components/footer/footer.tsx
+++ src/app/lib/components/footer/footer.tsx
@@ -1,9 +1,30 @@
+import { IconBrandGithub } from '@tabler/icons-react';
+import Link from 'next/link';
+
 const Footer: React.FC = () => {
-  return (
-    <div className="bg-yellow-600 py-12">
-      <h1 className="text-center text-xl">Footer</h1>
-    </div>
-  );
+    return (
+        <div className="p-4 bg-gray-800 text-white text-lg flex justify-between">
+            <div>
+                <h2 className="font-medium md:text-2xl">ALGO VISUALIZER</h2>
+                <Link
+                    href="https://my-portfolio-0327.web.app"
+                    className="hover:underline md:text-xl"
+                >
+                    Akshat Divya
+                </Link>
+            </div>
+            <div>
+                <h2 className="font-medium md:text-2xl">Reference</h2>
+                <Link
+                    href="https://github.com/Akshat42/algo-visualizer"
+                    className="flex gap-1 items-center"
+                >
+                    <IconBrandGithub stroke={2} />
+                    <p className="hover:underline md:text-xl">Github</p>
+                </Link>
+            </div>
+        </div>
+    );
 };
 
 export default Footer;
--- src/app/lib/components/sidebar/sidebar.tsx
+++ src/app/lib/components/sidebar/sidebar.tsx
@@ -6,34 +6,38 @@ import SidebarMenuItem from './sidebar-item/sidebar-menu-item';
 import { useSidebarStore } from 'lib/store/sidebar.store';
 
 const Sidebar: React.FC = () => {
-  const { isSideBarOpen } = useSidebarStore();
+    const { isSideBarOpen } = useSidebarStore();
 
-  function getSidebarClasses() {
-    if (isSideBarOpen) {
-      return 'h-full min-w-40 w-[15vw] absolute opacity-100 translate-y-0';
-    } else {
-      return 'hidden opacity-0 opacity-0 -translate-y-4';
+    function getSidebarClasses() {
+        if (isSideBarOpen) {
+            return 'h-full min-w-40 w-[15vw] absolute';
+        } else {
+            return 'hidden';
+        }
     }
-  }
-  return (
-    <aside className={getSidebarClasses()}>
-      <div className="h-full bg-gray-800 text-white overflow-auto pt-14 ">
-        {NavLinkList.map((navCategory) => {
-          return (
-            <div className="p-3 border-t-4 border-gray-600 ">
-              <div className="flex gap-2 p-1">
-                <IconMenu3 stroke={2} />
-                <h2 className="text-2xl font-bold">{navCategory.headerText}</h2>
-              </div>
-              {navCategory.subMenu.map((subMenuItem) => {
-                return <SidebarMenuItem menuItem={subMenuItem} />;
-              })}
+    return (
+        <aside className={getSidebarClasses()}>
+            <div className="h-full bg-gray-800 text-white overflow-auto pt-14 ">
+                {NavLinkList.map((navCategory) => {
+                    return (
+                        <div className="p-3 border-t-4 border-gray-600 ">
+                            <div className="flex gap-2 p-1 items-center">
+                                <IconMenu3 stroke={2} />
+                                <h2 className="text-2xl font-bold">
+                                    {navCategory.headerText}
+                                </h2>
+                            </div>
+                            {navCategory.subMenu.map((subMenuItem) => {
+                                return (
+                                    <SidebarMenuItem menuItem={subMenuItem} />
+                                );
+                            })}
+                        </div>
+                    );
+                })}
             </div>
-          );
-        })}
-      </div>
-    </aside>
-  );
+        </aside>
+    );
 };
 
 export default Sidebar;
